5b391b843c13dddc24e7e8398d9e94f239d99fba
Puneet <bhargavapuneet22@gmail.com>
2026-09-10 08:42:38 +0530

    Update dashboard browser title & added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/globals.css';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Onextel Dashboard</title>
+
+        <meta
+          name="description"
+          content="Onextel Campaign Dashboard"
+        />
+
+        <link
+          rel="icon"
+          href="/favicon.png"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
